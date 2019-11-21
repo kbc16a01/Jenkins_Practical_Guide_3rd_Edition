b35e61364a3984a3590263ba5bd4c605f653d2c8
kbc16a01 <kbc16a01@stu.kawahara.ac.jp>
2019-11-21 13:48:52 +0000

    [add]test

--- src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
+++ src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
@@ -79,7 +79,7 @@ public class SampleActionTest {
 	@Test
 	public void testExecuteNormal() {
 		SampleAction action = new SampleAction("firstName", "lastName");
-		HttpServletRequest request = createMock(HttpServletRequest.class);
+		// HttpServletRequest request = createMock(HttpServletRequest.class);
 		HttpSession session = createMock(HttpSession.class);
 
 		expect(request.getSession(true)).andReturn(session);
